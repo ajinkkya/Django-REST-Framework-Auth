c69f7a27cac9669317a0088533cc53f13c408127
ajinkyacswg <138433791+ajinkyacswg@users.noreply.github.com>
2023-07-30 22:49:14 +0530

    Sign Up API is Working
    User can sign up using username, password and email. and he will get token for it. password is be hashed.

--- server/views.py
+++ server/views.py
@@ -1,9 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . serializers import UserSerializers
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 @api_view(["POST"])
 def signup(request):
-    return Response({})
+    serializer = UserSerializers(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        
+        user = User.objects.get(username=request.data['username'])        
+        user.set_password(request.data['password'])
+        user.save()
+        
+        token = Token.objects.create(user=user)
+        
+        return Response(
+            {
+                "token" : token.key,
+                "user" : serializer.data
+            }
+        )
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+    )
 
 @api_view(["POST"])
 def login(request):
